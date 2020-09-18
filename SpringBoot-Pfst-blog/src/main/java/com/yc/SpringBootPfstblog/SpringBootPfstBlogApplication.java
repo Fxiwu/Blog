1c003eea52492df3e2a1520bd0b73b38d94bf252
13631 <13631@DESKTOP-3M75H1Q>
2020-09-18 21:08:18 +0800

    初始化项目工程

---
+++ SpringBoot-Pfst-blog/src/main/java/com/yc/SpringBootPfstblog/SpringBootPfstBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yc.SpringBootPfstblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPfstBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPfstBlogApplication.class, args);
+	}
+
+}
