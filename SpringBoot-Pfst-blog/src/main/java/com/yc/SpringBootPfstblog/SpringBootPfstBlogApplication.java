01a3a2c7a17a55d603e8df4f833e8406edfeaafe
13631 <13631@DESKTOP-3M75H1Q>
2020-09-24 20:47:38 +0800

    拦截器(发表文章)

--- SpringBoot-Pfst-blog/src/main/java/com/yc/SpringBootPfstblog/SpringBootPfstBlogApplication.java
+++ SpringBoot-Pfst-blog/src/main/java/com/yc/SpringBootPfstblog/SpringBootPfstBlogApplication.java
@@ -4,10 +4,23 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.yc.SpringBootPfstblog.web.LoginInterceptor;
 
 @SpringBootApplication
 @MapperScan("com.yc.SpringBootPfstblog")
-public class SpringBootPfstBlogApplication {
+public class SpringBootPfstBlogApplication implements WebMvcConfigurer{
+
+	@Override   //拦截器注册方法  参数：拦截器注册器
+	public void addInterceptors(InterceptorRegistry registry) {
+		 
+		WebMvcConfigurer.super.addInterceptors(registry);
+		InterceptorRegistration	ir=registry.addInterceptor(new LoginInterceptor());
+		ir.addPathPatterns("/toAddArticle","/addrticle.do");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPfstBlogApplication.class, args);
